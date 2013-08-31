24e4444ae47fc52595e7bebd2cd358ba50b105b5
Dan Heidel <dan.heidel@gmail.com>
2013-08-31 00:04:01 -0700

    first commit

--- config-sample.js
+++ config-sample.js
@@ -3,12 +3,12 @@ var config = {};
 config.couchdb = {};
 config.twilio = {};
 
-config.couchdb.url = 'https://couchserver';
+config.couchdb.url = 'https://danheidel.cloudant.com/events/';
 config.couchdb.port = 443;
-config.couchdb.username = 'xxx';
-config.couchdb.password = 'yyy';
+config.couchdb.username = 'danheidel';
+config.couchdb.password = 'dpPG0mr2yuhDUFRqCALb';
 
-config.twilio.sid = 'ACxxx';
-config.twilio.key = 'yyy';
+config.twilio.sid = 'ACa5acd0344417f1954dfc96edfc5bfafc';
+config.twilio.key = '3bc33573ada70ac387e71c0ac8ab84b1';
 
 module.exports = config;
